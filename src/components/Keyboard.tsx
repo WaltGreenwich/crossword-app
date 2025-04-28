e550e0815fb26620edbc0a7121e73926c2eb570e
WGreenwichChaca <walter.greenwich@gmail.com>
2025-04-27 19:10:01 -0500

    fixed keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -13,7 +13,7 @@ export default function Keyboard() {
   const keys = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Z", "X", "C", "V", "B", "N", "M", "⌫"],
+    ["Z", "X", "C", "V", "B", "N", "M", "˿"],
   ];
 
   const handleKeyPress = (key: string) => {
